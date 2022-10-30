265c9835049201b9a9ff2840299368c8a9c27c65
jacky chen <jackychen4@gmail.com>
2022-10-29 23:10:11 -0400

    package changes

--- server.py
+++ server.py
@@ -1,11 +1,34 @@
-from flask import Flask, request, jsonify, render_template
-
+from distutils.log import debug
+from flask import Flask, request, render_template
+from flaskwebgui import FlaskUI
+from sqlalchemy import true
 # Create the app object
 
 app = Flask(__name__, template_folder='templates')
+ui = FlaskUI(app, width=500, height=500)
 
-# importing function for calculations
-from calculator import basic_calculator
+def basic_calculator(a, b, operator):
+    if(a.isnumeric() and b.isnumeric()):
+        a = float(a)
+        b = float(b)
+        if(operator == '+'):
+            result = a + b
+        elif(operator == '-'):
+            result = a - b
+        elif (operator == 'x'):
+            result = a*b
+        elif(operator == '/'):
+            if(b == 0): result = "Please enter a valid value."
+            else: result = a/b
+        elif(operator == 'mod'):
+            result = a%b
+        elif(operator == "*"):
+            result = a**b
+        else:
+            result = "Please enter a valid value."
+    else:
+        result = 'Please enter a valid value.'
+    return result
 
 # Define calculator
 @app.route('/')
@@ -24,4 +47,5 @@ def calc():
     return render_template('index.html', text=str(result))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    ui.run()
+    
